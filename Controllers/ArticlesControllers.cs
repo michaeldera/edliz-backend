bd7b22cec64f0d35ad7032d4200a44572dc75018
Michael Dera <michaeldera@live.com>
2019-05-20 12:18:07 +0200

    modified ArticleController.

--- Controllers/ArticlesControllers.cs
+++ Controllers/ArticlesControllers.cs
@@ -19,20 +19,20 @@ namespace Edliz.Controllers
         }
         
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Article> Get(int id)
         {
-            return "value";
+            return new Article();
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Article value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Article value)
         {
         }
 
